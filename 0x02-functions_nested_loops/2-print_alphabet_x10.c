9ec097675f914829b924a32a38026f448679afb4
Radzijnr <nhlamuloradzilani@gmail.com>
2023-03-21 14:01:06 -0700

    initial

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Entry point
+ * Description: program prints alphabet 10 times without stdlib
+ * Return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int v;
+	char g;
+
+	for (v = 0; v <= 10; v++)
+	{
+		for (g = 'a'; g <= 'z'; g++)
+		{
+		_putchar(v);
+		}
+	       _putchar('\n');
+	}
+}
