09c3f5c551b6ecba358afbc1a08c2644957b2084
Radzijnr <nhlamuloradzilani@gmail.com>
2023-03-21 12:01:17 -0700

    initial

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,13 +6,15 @@
  */
 
 void print_alphabet(void);
-{
-		char c = 'a';
 
-		while (c <= 'z')
+{
+		char c = 'a'; {
+
+		while c <= 'z';
 		{
 			_putchar(c);
 			c++;
 		}
 		_putchar('\n');
+		return (0);
 }
