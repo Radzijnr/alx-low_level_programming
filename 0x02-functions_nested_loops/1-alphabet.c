0d547e88c2e40a3df7d53c57bf0c9230a7abbb03
Radzijnr <nhlamuloradzilani@gmail.com>
2023-03-21 11:03:17 -0700

    initial

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - function prints alphabet,
+ * Return: 0.
+ */
+
+void print_alphabet(void);
+{
+		char c = 'a';
+
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
+}
