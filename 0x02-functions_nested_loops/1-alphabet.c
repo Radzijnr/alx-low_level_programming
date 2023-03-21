2fdc89dc347b114cb4ee1dd80985939c8edee5c3
Radzijnr <nhlamuloradzilani@gmail.com>
2023-03-21 13:11:53 -0700

    added entry point

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,13 @@
 #include "main.h"
 
 /**
- * main - function prints alphabet,
+ * print_alphabet - Entry point
+ * Description: The program prints alphabets without stdlib
  * Return: 0.
  */
 
 void print_alphabet(void)
+/* declaration of print_alphabet */
 {
 
 		char c = 'a';
